d3937d8d80051c95f09a46ded6130d52424b0c71
Raul Glogovetan <raul@glogovetan.com>
2019-01-06 21:26:48 +0200

    Add: mailchimp newsletter capture form

--- _src/augment.js
+++ _src/augment.js
@@ -1,13 +1,29 @@
 /*
- * Very rough way to get the server's time
+ * Event listener hooken onto the Mailchimp Input
  *
- * Basically:
- * - Make a XHR request to the current server
- * - Get the `Date` response header
- * - Use that value to create a new Date Object and get the full year
- * - After the page is loaded set that to the element's id
+ * If it has content (value !== null) then show the submit button,
+ * otherwise hide it
  */
+const toggleMailchimpButton = () => {
+  const mailchimpInput = document.getElementById('mailchimpInput');
+  const mailchimpSubmitWrapper = document.getElementById(
+    'mailchimpSumitButton',
+  );
+  mailchimpSubmitWrapper.style.display = mailchimpInput.value
+    ? 'block'
+    : 'none';
+};
+
 window.onload = () => {
+  /*
+   * Very rough way to get the server's time
+   *
+   * Basically:
+   * - Make a XHR request to the current server
+   * - Get the `Date` response header
+   * - Use that value to create a new Date Object and get the full year
+   * - After the page is loaded set that to the element's id
+   */
   const xhr = new XMLHttpRequest();
   xhr.open('HEAD', window.location.href.toString(), true);
   xhr.setRequestHeader('Content-Type', 'text/html');
@@ -18,4 +34,11 @@ window.onload = () => {
     ).getFullYear();
   };
   xhr.send(null);
+
+  /*
+   * Add a new event listener on the Mailchimp input
+   */
+  document
+    .getElementById('mailchimpInput')
+    .addEventListener('input', toggleMailchimpButton);
 };
